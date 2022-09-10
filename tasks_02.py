739eb0ee6e68d32e208954e67f9a516a1ef86b07
Pavel Pecherskih <pecherskih@netcracker.com>
2022-09-10 20:43:21 +0400

    changes from seminar

--- tasks_02.py
+++ tasks_02.py
@@ -3,9 +3,11 @@ print('task 01')
 n = input('Write a number: ')
 sum = 0
 for i in n:
+    #if i.isdigit():
     if(i!='.' and i!=','):
         sum += int(i)
 print(sum)
+#print(sum([int(i) for i in input() if i.isdigit()]))
 
 #2. Напишите программу, которая принимает на вход число N 
 #  и выдает набор произведений чисел от 1 до N.
@@ -17,9 +19,12 @@ print(sum)
 print('task_02')
 n = int(input('Write a number: '))
 factorial = 1
+#some_list=[]
 for i in range (1, n + 1):
+    #some_list.append(factorial*i)
     factorial *= i
     print(factorial, end = ' ')
+#print(some_list)
 print()
 
 #3.Задайте список из n чисел последовательности (1 + 1/n)**n и выведите на экран их сумму.
@@ -37,12 +42,21 @@ print(sum)
 # Найдите произведение элементов на указанных позициях.
 #  Позиции хранятся в файле file.txt в одной строке одно число.
 print('task_04')
-n = int(input('Write a number: '))
-list = [i for i in range(-n,n+1)]
-print(list)
-#Запуталась и не понимаю. Нужно вручную прописывать позиции (индексы) в файле 
-# или сделать это автоматически при создании списка... Если вручную, то получается стремно, 
-# зачем вообще нужен файл. Если автоматически, то не понимаю, как сделать.
+from random import *
+n = int(input())
+some_list = [randint(-n,n) for _ in range(randint(5,10))]
+print(some_list)
+
+f = open('file.txt', 'w')
+for i in range(randint(2, len(some_list))):
+    f.write(str(randint(0, len(some_list) - 1)) + '\n')
+f.close()
+
+pr = 1
+with open('file.txt', 'r') as f:
+    for i in f.read().splitlines():
+        pr = pr*some_list[int(i)]
+print(pr)
 
 
 #5. Реализуйте алгоритм перемешивания списка.
